130033a4925cf763b1d3a75c137d040607e1f0b4
James Di <xinjie_di@apple.com>
2015-11-08 12:48:44 -0800

    definition for person and loc

---
+++ Service/shared/schemaHelper.js
@@ -0,0 +1,34 @@
+
+
+exports.person = function() {
+	return {
+	    name: String,
+	    birthDate: Date,
+	    gender: {type: String, enum: ['Male', 'Female']},
+	    email: {type: String, default: ''},
+	    phones: [{label: String, number: String}],
+	    userName: String,
+	    hashed_password: { type: String, default: '' },
+  	    salt: { type: String, default: '' },
+  	    socialAccounts: [{acount: String, userName: String, hashed_password: String}]
+    };
+};
+
+exports.loc = function() {
+	return {
+		type: {
+			type: String
+		},
+		coordinates: []
+	};
+};
+
+exports.address = function() {
+	return {
+		line1: String,
+		line2: String,
+		city: String,
+		province: String,
+		zip: String
+	};
+};
