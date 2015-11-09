97df9c807c3e80e9541f2f618ce397c2666c43c1
Shuyun Lei <slei@slei-ltm2.internal.salesforce.com>
2015-11-08 17:24:18 -0800

    add tagbar

--- client/jiyi/index.ios.js
+++ client/jiyi/index.ios.js
@@ -8,24 +8,58 @@ var React = require('react-native');
 var {
     AppRegistry,
     StyleSheet,
-    NavigatorIOS,
+    Navigator,
+    View,
+    NavigatorIOS
 } = React;
 
 var InspireScreen = require('./InspireScreen');
 
 var jiyi = React.createClass({
+    // renderScene: function(route, navigator) {
+    //     var Component = route.component;
+    //     return (
+    //         <View style={styles.container}>
+    //             <Component
+    //                 route={route}
+    //                 navigator={navigator}
+    //                 topNavigator={navigator} />
+    //         </View>
+    //     )
+    // },
+
     render: function() {
         return (
             <NavigatorIOS
+                //renderScene={this.renderScene}
                 initialRoute={{
                     component: InspireScreen,
-                    title: 'Choose Topic'
+                    title: 'Choose Tag'
                 }}
                 style={styles.container}
             />
         );
     }
+
+    // render: function() {
+    //     return (
+    //         <Navigator
+    //             //ref={(navigator) => { this.navigator = navigator; }}
+    //             renderScene={this.renderScene}
+    //             tintColor='#D6573D'
+    //             barTintColor='#FFFFFD'
+    //             titleTextColor='#D6573D'
+    //             navigationBarHidden={false}
+    //             initialRoute={{
+    //                 component: InspireScreen,
+    //                 title: 'Choose Tag'
+    //             }}
+    //             sceneStyle={styles.container}
+    //         />
+    //     );
+    // }
 });
+
 
 var styles = StyleSheet.create({
     container: {
